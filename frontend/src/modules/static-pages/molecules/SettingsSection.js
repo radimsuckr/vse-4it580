0593c004057522af33e110f5860dbfe89889f94a
Radim Sückr <radim.suckr@gmail.com>
2022-10-10 17:18:34 +0200

    feat: somewhat in progress validations

--- frontend/src/modules/static-pages/molecules/SettingsSection.js
+++ frontend/src/modules/static-pages/molecules/SettingsSection.js
@@ -8,8 +8,17 @@ import {
 } from 'src/shared/design-system';
 
 import { Form } from 'src/shared/hook-form';
+import { FormProvider } from "react-hook-form";
 
 export function SettingsSection({ formProps, title, description, children }) {
+  const handleSuccess = (data) => {
+    console.log('✅ submit success', data);
+  };
+
+  const handleError = (errors) => {
+    console.log('⛔️ submit error', errors);
+  };
+
   return (
     <Stack direction={['column', 'column', 'column', 'row']}>
       <Box w={['100%', '100%', '100%', '33%']}>
@@ -23,15 +32,22 @@ export function SettingsSection({ formProps, title, description, children }) {
         rounded='md'
         borderRadius='md'
       >
-        <Form {...formProps}>
-          <Stack bg="white">
-            <Box p='4'>
-              {children}
-            </Box>
-            <Box textAlign="right" backgroundColor='rgb(247, 249, 252)' p='4'>
-              <Button type="submit" colorScheme='green'>Save</Button>
-            </Box>
-          </Stack>
+        <Form
+          onSubmit={formProps.handleSubmit(
+            handleSuccess,
+            handleError,
+          )}
+        >
+          <FormProvider {...formProps}>
+            <Stack bg="white">
+              <Box p='4'>
+                {children}
+              </Box>
+              <Box textAlign="right" backgroundColor='rgb(247, 249, 252)' p='4'>
+                <Button type="submit" colorScheme='green'>Save</Button>
+              </Box>
+            </Stack>
+          </FormProvider>
         </Form>
       </Box>
     </Stack >
--- frontend/src/modules/static-pages/pages/Practical03Page.js
+++ frontend/src/modules/static-pages/pages/Practical03Page.js
@@ -17,10 +17,84 @@ import {
 } from 'src/shared/design-system';
 
 import { FormField } from 'src/shared/hook-form';
-
+import { useController, useForm, FormProvider } from "react-hook-form";
 import { SettingsSection } from '../molecules';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const contactFormSchema = yup.object().shape({
+  firstName: yup.string().min(2).max(20).required(),
+  lastName: yup.string().min(2).max(20).required(),
+  username: yup.string().min(2).max(20).required(),
+  email: yup
+    .string()
+    .email()
+    .test(
+      'CZ domain',
+      'E-mail must end with .cz',
+      (value) => value.endsWith('.cz'),
+    )
+    .required(),
+  about: yup.string().max(1000).required(),
+  agreeToc: yup.boolean().isTrue().required(),
+});
+
+function InputField({ name, ...props }) {
+  const { field, fieldState } = useController({
+    name,
+  });
+
+  return <Input {...props} {...field} isInvalid={!!fieldState.error} />;
+}
+
+function TextareaField({ name, ...props }) {
+  const controller = useController({
+    name,
+  });
+
+  return <Textarea {...props} {...controller.field} />;
+}
+
+function SelectField({ name, values, ...props }) {
+  const controller = useController({
+    name,
+  });
+
+  return (
+    <Select name={name} {...props} {...controller.field}>
+      {values.map((v) => (<option key={v} value={v}>{v}</option>))}
+    </Select>
+  );
+}
+
+function SwitchField({ name, message, ...props }) {
+  const controller = useController({
+    name,
+  });
+
+  return <Switch {...props} {...controller.field}>{message}</Switch>;
+}
 
 export function Practical03Page() {
+  const form1Props = useForm({
+    defaultValues: {
+      firstName: 'John',
+      lastName: 'Doe',
+      username: 'jdoe',
+      email: 'john@doe.com',
+      about: 'Lorem ipsum',
+      agreeToc: true,
+    },
+    mode: 'onBlur',
+    resolver: yupResolver(contactFormSchema),
+  });
+
+  const form2Props = useForm({
+    defaultValues: {
+      notificationsLevel: 'mentions',
+    },
+  });
+
   return (
     <>
       <BodyBackground bg="gray.100" />
@@ -30,86 +104,63 @@ export function Practical03Page() {
         <SettingsSection
           title="Profile"
           description="This is your profile information."
-          formProps={{
-            defaultValues: {
-              firstName: 'John',
-              lastName: 'Doe',
-              username: 'jdoe',
-              email: 'john@doe.com',
-              about: 'Lorem ipsum',
-              agreeToc: true,
-            },
-            onSubmit: (data) => {
-              alert(JSON.stringify(data, null, 2));
-            },
-          }}
+          formProps={form1Props}
         >
           <Stack mb='4' direction='row'>
             <Box w='50%'>
               <FormControl>
                 <FormLabel>First name</FormLabel>
-                <Input type='text' />
+                <InputField name='firstName' type='text' />
               </FormControl>
             </Box>
             <Box w='50%'>
               <FormControl>
                 <FormLabel>Last name</FormLabel>
-                <Input type='text' />
+                <InputField name='lastName' type='text' />
               </FormControl>
             </Box>
           </Stack>
           <Box mb='4' w='55%'>
             <FormControl>
               <FormLabel>User name</FormLabel>
-              <Input type='text' />
+              <InputField name='username' type='text' />
             </FormControl>
           </Box>
           <Box mb='4' w='55%'>
             <FormControl>
               <FormLabel>Email address</FormLabel>
-              <Input type='email' />
+              <InputField name='email' type='email' />
             </FormControl>
           </Box>
           <Box mb='4' w='100%'>
             <FormControl>
               <FormLabel>Profile bio</FormLabel>
-              <Textarea />
+              <TextareaField name='about' />
             </FormControl>
           </Box>
           <Box mb='4' w='100%'>
             <FormControl>
               <FormLabel>Profile visibility</FormLabel>
-              <Select>
-                <option value="public">Public</option>
-                <option value="friends">Only friends</option>
-                <option value="private">Private</option>
-              </Select>
+              <SelectField name='visibility' values={['Public', 'Only friends', 'Private']} />
             </FormControl>
           </Box>
           <Box w='100%'>
-            <Switch> Agree to Terms and Conditions</Switch>
+            <SwitchField name='agreeToc' message=' Agree to Terms and Conditions' />
           </Box>
         </SettingsSection>
         <StackDivider p='4' />
         <SettingsSection
           title="Notifications"
           description="Setup how much notification you will receive"
-          formProps={{
-            defaultValues: {
-              notificationsLevel: 'mentions',
-            },
-            onSubmit: (data) => {
-              alert(JSON.stringify(data, null, 2));
-            },
-          }}
+          formProps={form2Props}
         >
           <RadioGroup>
             <Heading as="h5">Notify me</Heading>
             <Paragraph>When you should be notified:</Paragraph>
             <Stack pl='4'>
-              <Radio value="all">Every time someone quacks</Radio>
-              <Radio value="mentions">Only mentions (@username)</Radio>
-              <Radio value="never">Never</Radio>
+              <Radio name='notificationsLevel' value="all">Every time someone quacks</Radio>
+              <Radio name='notificationsLevel' value="mentions">Only mentions (@username)</Radio>
+              <Radio name='notificationsLevel' value="never">Never</Radio>
             </Stack>
           </RadioGroup>
         </SettingsSection>
